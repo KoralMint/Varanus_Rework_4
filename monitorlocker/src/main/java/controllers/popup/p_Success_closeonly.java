6428ee47e039e0ae5c95954e2e21d0e79b795670
KoralMint <aitaiyosora300@gmail.com>
2024-06-13 11:50:53 +0900

    popup作っただけ

---
+++ monitorlocker/src/main/java/controllers/popup/p_Success_closeonly.java
@@ -0,0 +1,27 @@
+package controllers.popup;
+
+import Application.Main;
+
+public class p_Success_closeonly implements controllers.common.Popup {
+
+    
+
+    @Override
+    public void updateKeyBinding() {
+        Main.PRIMARYSTAGE.getScene().setOnKeyPressed(e -> {
+            switch(e.getCode()){
+                // Z X, J L I K
+                // B R, < > ^ v
+                case Z: // Blue
+                    System.out.println("Blue key pressed");
+                    break;
+                case X: // Red
+                    System.out.println("Red key pressed");
+                    break;
+
+                default:
+                    break;
+            }
+        });
+    }
+}
